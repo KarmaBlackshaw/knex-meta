85fa5a0972508bd50c439c875f75924172d4c173
Karma Blackshaw <irenesejah29@gmail.com>
2022-07-04 16:20:35 +0800

    add types for new query builder methods

---
+++ @types/knex.d.ts
@@ -0,0 +1,18 @@
+import { Knex as KnexOriginal } from 'knex';
+import { IDateArguments } from './metaDate'
+import { IFilterArguments } from './metaFilter'
+import { ISortArguments } from './metaSort'
+import { IPageArguments } from './metaPage'
+import { IMetaArguments } from './meta'
+
+declare module 'knex' {
+  namespace Knex {
+    interface QueryBuilder {
+      meta<TRecord, TResult>(IMetaArguments): KnexOriginal.QueryBuilder<TRecord, TResult>;
+      metaDate<TRecord, TResult>(IDateArguments): KnexOriginal.QueryBuilder<TRecord, TResult>;
+      metaFilter<TRecord, TResult>(IFilterArguments): KnexOriginal.QueryBuilder<TRecord, TResult>;
+      metaSort<TRecord, TResult>(ISortArguments): KnexOriginal.QueryBuilder<TRecord, TResult>;
+      metaPage<TRecord, TResult>(IPageArguments): KnexOriginal.QueryBuilder<TRecord, TResult>;
+    }
+  }
+}
