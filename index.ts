ab8be1f5d0c86b13e587573b43fbc3ab79de0886
karmablackshaw <irenesejah29@gmail.com>
2022-07-09 19:46:48 +0800

    add bulkupdate

---
+++ index.ts
@@ -0,0 +1,26 @@
+
+
+// libs
+import { knex } from 'knex'
+
+import metaDate from './src/metaDate'
+import metaFilter from './src/metaFilter'
+import metaPage from './src/metaPage'
+import metaSort from './src/metaSort'
+import meta from './src/meta'
+import bulkUpdate from './src/bulkUpdate'
+
+const extensions = [
+  metaDate,
+  metaFilter,
+  metaPage,
+  metaSort,
+  meta,
+  bulkUpdate
+]
+
+extensions.forEach(extension => {
+  knex.QueryBuilder.extend(extension.name, extension)
+})
+
+export default knex
