84e9865d38eee48ae462820eebd50e24805d6796
Ernie Jeash Villahermosa <irenesejah29@gmail.com>
2023-03-13 14:58:32 +0800

    test

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-/**Do not change, this is programmatically updated**/
+/** Do not change, this is programmatically updated**/
 
-  import { bulkUpdate } from './core/bulkUpdate'
+import { bulkUpdate } from './core/bulkUpdate'
 import { meta } from './core/meta'
 import { metaDate } from './core/metaDate'
 import { jsonObject } from './core/jsonObject'
@@ -12,19 +12,18 @@ import { metaQuery } from './core/metaQuery'
 import { metaSort } from './core/metaSort'
 import { metaUpdate } from './core/metaUpdate'
 
-  export default knex => {
-    knex.QueryBuilder.extend(bulkUpdate.name, bulkUpdate)
-knex.QueryBuilder.extend(meta.name, meta)
-knex.QueryBuilder.extend(metaDate.name, metaDate)
-knex.QueryBuilder.extend(jsonObject.name, jsonObject)
-knex.QueryBuilder.extend(metaFilter.name, metaFilter)
-knex.QueryBuilder.extend(metaFind.name, metaFind)
-knex.QueryBuilder.extend(metaInsert.name, metaInsert)
-knex.QueryBuilder.extend(metaPage.name, metaPage)
-knex.QueryBuilder.extend(metaQuery.name, metaQuery)
-knex.QueryBuilder.extend(metaSort.name, metaSort)
-knex.QueryBuilder.extend(metaUpdate.name, metaUpdate)
+export default knex => {
+  knex.QueryBuilder.extend(bulkUpdate.name, bulkUpdate)
+  knex.QueryBuilder.extend(meta.name, meta)
+  knex.QueryBuilder.extend(metaDate.name, metaDate)
+  knex.QueryBuilder.extend(jsonObject.name, jsonObject)
+  knex.QueryBuilder.extend(metaFilter.name, metaFilter)
+  knex.QueryBuilder.extend(metaFind.name, metaFind)
+  knex.QueryBuilder.extend(metaInsert.name, metaInsert)
+  knex.QueryBuilder.extend(metaPage.name, metaPage)
+  knex.QueryBuilder.extend(metaQuery.name, metaQuery)
+  knex.QueryBuilder.extend(metaSort.name, metaSort)
+  kndex.QueryBuilder.extend(metaUpdate.name, metaUpdate)
 
-
-    return knex
-  }
+  return knex
+}
