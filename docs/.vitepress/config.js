767a11fb4188767784dfb448012c47f0e2311c05
Ernie Jeash Villahermosa <irenesejah29@gmail.com>
2023-03-17 15:13:36 +0800

    chore: update docs

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -10,39 +10,58 @@ export default defineConfig(async () => {
   })
 
   return {
-    title: 'Knex Meta',
-    description: 'Utility extensions for knex query builder',
-    lang: 'en-US',
+    title: "Knex Meta",
+    lastUpdated: true,
+    description: "Utility extensions for knex query builder",
+    lang: "en-US",
     themeConfig: {
-      siteTitle: 'Knex Meta',
+      siteTitle: "Knex Meta",
+      socialLinks: [
+        { icon: "github", link: "https://github.com/KarmaBlackshaw/knex-meta" },
+        {
+          icon: "linkedin",
+          link: "https://www.linkedin.com/in/ernie-jeash-villahermosa-576187199/",
+        },
+        {
+          icon: "facebook",
+          link: "https://www.facebook.com/KarmaBlackshaw/",
+        },
+        {
+          icon: "discord",
+          link: "https://discord.gg/kMq3PrXaCP",
+        },
+      ],
+
       nav: [
-        { text: 'Guide', link: '/guide/what-is-knex-meta' },
-        { text: 'Config', link: '/config' },
-        { text: 'Changelog', link: 'https://github.com/KarmaBlackshaw/knex-meta/blob/master/CHANGELOG.md' }
+        { text: "Guide", link: "/guide/what-is-knex-meta" },
+        {
+          text: "Changelog",
+          link: "https://github.com/KarmaBlackshaw/knex-meta/blob/master/CHANGELOG.md",
+        },
       ],
       sidebar: [
         {
-          text: 'Guide',
+          text: "Guide",
           items: [
-            { text: 'What is Knex Meta', link: '/guide/what-is-knex-meta' },
-            { text: 'Getting Started', link: '/guide/getting-started' },
-          ]
+            { text: "What is Knex Meta", link: "/guide/what-is-knex-meta" },
+            { text: "Getting Started", link: "/guide/getting-started" },
+          ],
         },
         {
-          text: 'Utilities',
-          items: utilities.map(util => {
-            const name = util.replace('.md', '')
+          text: "Utilities",
+          items: utilities.map((util) => {
+            const name = util.replace(".md", "")
 
             return {
               text: name,
-              link: `/utility/${name}`
+              link: `/utility/${name}`,
             }
-          })
-        }
+          }),
+        },
       ],
       footer: {
-        copyright: 'Copyright © 2019-present Ernie Jeash Villahermosa'
-      }
-    }
+        copyright: "Copyright © 2019-present Ernie Jeash Villahermosa",
+      },
+    },
   }
 })
--- docs/guide/getting-started.md
+++ docs/guide/getting-started.md
@@ -6,9 +6,36 @@ layout: doc
 
 ## Installation
 
-### Prerequisits
-- [NodeJS](https://nodejs.org/en/)
+::: tip Prerequisites
 
-```sh
+- [Mysql](https://www.npmjs.com/package/mysql)
+- [Knex.js](https://www.npmjs.com/package/knex)
+:::
+
+::: code-group
+```sh [npm]
 $ npm i @jeash/knex-meta
+```
+```sh [pnpm]
+$ pnpm add @jeash/knex-meta
+```
+```sh [yarn]
+$ yarn add @jeash/knex-meta
+```
+:::
+
+## Usage
+```js
+const Knex = require('knex')
+const knexMeta = require('@jeash/knex-meta')
+
+const knex = knexMeta(Knex)
+
+export default knex({
+  client: 'mysql',
+  connection: {
+    ...
+  }
+})
+
 ```
