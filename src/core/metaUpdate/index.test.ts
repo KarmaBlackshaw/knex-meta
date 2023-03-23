435bd92b21cf5ffa3a1db7a6ddfec409c3c1bafb
Ernie Jeash Villahermosa <irenesejah29@gmail.com>
2023-03-23 18:53:44 +0800

    chore: update metaUpdate

--- src/core/metaUpdate/index.test.ts
+++ src/core/metaUpdate/index.test.ts
@@ -2,62 +2,135 @@ import { expect, test } from 'vitest'
 
 import knex from '../../connection'
 
-test('Should perform simple update', () => {
-  const dictionary = {
-    name: 'users.name'
+test('Update with an object payload', () => {
+  const updateData = {
+    id: 1,
+    name: 'John',
+    username: 30
   }
 
-  const payload = {
-    name: 'Jeash'
+  const options = {
+    fields: {
+      id: 'users.id',
+      name: 'users.name',
+      username: 'users.username'
+    }
   }
 
   const result = knex('users')
-    .where('users.id', 1)
-    .metaUpdate(payload, dictionary)
+    .metaUpdate('id', updateData, options)
     .toString()
 
-  const expected = "update `users` set `users`.`name` = 'Jeash' where `users`.`id` = 1"
+  const expected = "update `users` set `name` = CASE WHEN (users.id = 1) THEN 'John'  END, `username` = CASE WHEN (users.id = 1) THEN 30  END where ((`users`.`id` = 1))"
 
   expect(result).toBe(expected)
 })
 
-test('Should not include keys not in the dictionary', () => {
-  const dictionary = {
-    name: 'users.name'
+test('Update with an object payload and else condition', () => {
+  const updateData = {
+    id: 1,
+    name: 'John',
+    username: 30
   }
 
-  const payload = {
-    name: 'Jeash',
-    age: 12
+  const options = {
+    fields: {
+      id: 'users.id',
+      name: 'users.name',
+      username: 'users.username'
+    },
+    else: {
+      name: 'foo'
+    }
   }
 
   const result = knex('users')
-    .where('users.id', 1)
-    .metaUpdate(payload, dictionary)
+    .metaUpdate('id', updateData, options)
     .toString()
 
-  const expected = "update `users` set `users`.`name` = 'Jeash' where `users`.`id` = 1"
+  const expected = "update `users` set `name` = CASE WHEN (users.id = 1) THEN 'John' ELSE 'foo' END, `username` = CASE WHEN (users.id = 1) THEN 30  END where ((`users`.`id` = 1))"
 
   expect(result).toBe(expected)
 })
 
-test('Should not include undefined values', () => {
-  const dictionary = {
-    name: 'users.name',
-    age: 'users.age'
+test('Update with an object whose key does not exist in the fields', () => {
+  const updateData = {
+    id: 1,
+    name: 'John',
+    username: 30
   }
 
-  const payload = {
-    name: 'Jeash',
-    age: undefined
+  const options = {
+    fields: {
+      id: 'users.id',
+      name: 'users.name'
+    }
   }
 
   const result = knex('users')
-    .where('users.id', 1)
-    .metaUpdate(payload, dictionary)
+    .metaUpdate('id', updateData, options)
     .toString()
 
-  const expected = "update `users` set `users`.`name` = 'Jeash' where `users`.`id` = 1"
+  const expected = "update `users` set `name` = CASE WHEN (users.id = 1) THEN 'John'  END where ((`users`.`id` = 1))"
+
+  expect(result).toBe(expected)
+})
+
+test('Update with multiple data', () => {
+  const updateData = [
+    {
+      id: 1,
+      name: 'John'
+    },
+    {
+      id: 2,
+      name: 'Mark'
+    }
+  ]
+
+  const options = {
+    fields: {
+      id: 'users.id',
+      name: 'users.name'
+    }
+  }
+
+  const result = knex('users')
+    .metaUpdate('id', updateData, options)
+    .toString()
+
+  const expected = "update `users` set `name` = CASE WHEN (users.id = 1) THEN 'John'  WHEN (users.id = 2) THEN 'Mark'  END where ((`users`.`id` = 1) or (`users`.`id` = 2))"
+
+  expect(result).toBe(expected)
+})
+
+test('Update with multiple keys', () => {
+  const updateData = [
+    {
+      id: 1,
+      name: 'John',
+      balance: 30
+    },
+    {
+      id: 2,
+      name: 'Mark',
+      balance: 25
+    }
+  ]
+
+  const options = {
+    fields: {
+      id: 'users.id',
+      name: 'users.name',
+      balance: 'users.balance'
+    }
+  }
+
+  const result = knex('users')
+    .metaUpdate(['id', 'name'], updateData, options)
+    .toString()
+
+  const expected = "update `users` set `balance` = CASE WHEN (users.id = 1 AND users.name = 'John') THEN 30  WHEN (users.id = 2 AND users.name = 'Mark') THEN 25  END where ((`users`.`id` = 1 and `users`.`name` = 'John') or (`users`.`id` = 2 and `users`.`name` = 'Mark'))"
 
   expect(result).toBe(expected)
 })
--- src/core/metaUpdate/index.ts
+++ src/core/metaUpdate/index.ts
@@ -1,39 +1,100 @@
-/**
- * UTILITIES
- */
+// libs
 import _ from 'lodash'
 
-type TPayload = Record<string, any>
-type TDictionary = Record<string, string>
+// types
+type dynamicObject = Record<string, any>
+type TBulkUpdateKey = string | string[]
+type TBulkUpdatePayload = dynamicObject | dynamicObject[]
+type TBulkUpdateOptions = {
+  fields: dynamicObject,
+  else?: dynamicObject
+}
 
-export function metaUpdate (payload: TPayload, dictionary: TDictionary) {
-  if (!payload || !dictionary) {
-    if (!payload) {
-      throw new TypeError('Payload of undefined is not permitted')
-    }
-
-    if (!dictionary) {
-      throw new TypeError('Dictionary of undefined is not permitted')
-    }
+// helpers
+const quoter = (foo: any) => {
+  if (typeof foo === 'number') {
+    return foo
   }
 
-  const updateData: TPayload = {}
-
-  for (const key in payload) {
-    const updateValue = payload[key]
-    const currDictionary = dictionary[key]
-
-    if (updateValue === undefined || !currDictionary) {
-      continue
-    }
-
-    updateData[currDictionary] = updateValue
+  if (typeof foo === 'string') {
+    return `'${foo}'`
   }
 
-  if (_.isEmpty(updateData)) {
-    return this
+  /**
+   * for raw queries
+   */
+  if (foo?.sql) {
+    return foo.sql
+  }
+
+  console.warn(`${typeof foo} is not accounted.`)
+}
+
+export function metaUpdate (
+  keyConditions: TBulkUpdateKey,
+  payload: TBulkUpdatePayload = [],
+  options: TBulkUpdateOptions
+) {
+  const keyConditionsArray = _.castArray(keyConditions)
+  const payloadArray = _.castArray(payload)
+  const fields = options.fields || {}
+  const elses = options.else || {}
+
+  const whereConditions = []
+  const caseConditionsByField = {}
+
+  payloadArray.forEach(currPayload => {
+    const condition = {}
+
+    keyConditionsArray.forEach(key => {
+      if (!fields[key]) {
+        console.warn(`[WARNING] bulkUpdates: "${key}" is not defined in fields`)
+      }
+
+      condition[fields[key]] = currPayload[key]
+
+      /**
+       * Do not update condition keys
+       */
+      delete currPayload[key]
+    })
+
+    whereConditions.push(condition)
+
+    const whenCondition = Object
+      .entries(condition)
+      .map(([field, value]) => `${field} = ${quoter(value)}`)
+      .join(' AND ')
+
+    for (const field in currPayload) {
+      /**
+       * Exclude fields which are not defined
+       */
+      if (!fields[field]) {
+        continue
+      }
+
+      if (!caseConditionsByField[field]) {
+        caseConditionsByField[field] = []
+      }
+
+      const elseStr = elses[field] ? `ELSE ${quoter(elses[field])}` : ''
+      const whenStr = `WHEN (${whenCondition}) THEN ${quoter(currPayload[field])} ${elseStr}`
+      caseConditionsByField[field].push(whenStr)
+    }
+  })
+
+  for (const field in caseConditionsByField) {
+    const fieldCases = caseConditionsByField[field]
+
+    caseConditionsByField[field] = this.client.raw(`CASE ${fieldCases.join(' ')} END`)
   }
 
   return this
-    .update(updateData)
+    .where(function () {
+      whereConditions.forEach(condition => {
+        this.orWhere(condition)
+      })
+    })
+    .update(caseConditionsByField)
 }
