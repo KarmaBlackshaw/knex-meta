730ed99cbb8edc67854fbe7e534acc04f79ea73e
Ernie Jeash Villahermosa <irenesejah29@gmail.com>
2023-04-14 18:37:08 +0800

    chore: add meta

---
+++ src/core/meta/index.test.ts
@@ -0,0 +1,103 @@
+import { expect, test } from 'vitest'
+
+import knex from '../../connection'
+
+test('Date meta functions correctly', () => {
+  const dictionary = {
+    birthdate: 'users.birthdate'
+  }
+
+  const result = knex('users')
+    .meta({
+      dateBy: 'birthdate',
+      dateFrom: '1998-07-29',
+      dateTo: '1998-07-29',
+      dateDictionary: dictionary
+    })
+    .toString()
+
+  const expected = "select * from `users` where `users`.`birthdate` >= '1998-07-29 00:00:00' and `users`.`birthdate` <= '1998-07-29 23:59:59'"
+
+  expect(result).toBe(expected)
+})
+
+test('Page meta functions correctly', () => {
+  const result = knex('users')
+    .meta({
+      page: 3,
+      rows: 50
+    })
+    .toString()
+
+  const expected = 'select * from `users` limit 50 offset 100'
+
+  expect(result).toBe(expected)
+})
+
+test('Sort meta functions correctly', () => {
+  const dictionary = {
+    birthdate: 'users.birthdate'
+  }
+
+  const result = knex('users')
+    .meta({
+      sort: 'asc',
+      sortBy: 'birthdate',
+      sortDictionary: dictionary
+    })
+    .toString()
+
+  const expected = 'select * from `users` order by `users`.`birthdate` asc'
+
+  expect(result).toBe(expected)
+})
+
+test('Filter meta functions correctly', () => {
+  const dictionary = {
+    birthdate: 'users.birthdate'
+  }
+
+  const result = knex('users')
+    .meta({
+      filterBy: 'birthdate',
+      q: 'july',
+      filterDictionary: dictionary
+    })
+    .toString()
+
+  const expected = "select * from `users` where `users`.`birthdate` like '%july%'"
+
+  expect(result).toBe(expected)
+})
+
+test('All meta functions correctly', () => {
+  const dictionary = {
+    birthdate: 'users.birthdate'
+  }
+
+  const result = knex('users')
+    .meta({
+      dateBy: 'birthdate',
+      dateFrom: '1998-07-29',
+      dateTo: '1998-07-29',
+      dateDictionary: dictionary,
+
+      page: 3,
+      rows: 50,
+      isCount: false,
+
+      sort: 'asc',
+      sortBy: 'birthdate',
+      sortDictionary: dictionary,
+
+      filterBy: 'birthdate',
+      q: 'july',
+      filterDictionary: dictionary
+    })
+    .toString()
+
+  const expected = "select * from `users` where `users`.`birthdate` >= '1998-07-29 00:00:00' and `users`.`birthdate` <= '1998-07-29 23:59:59' and `users`.`birthdate` like '%july%' order by `users`.`birthdate` asc limit 50 offset 100"
+
+  expect(result).toBe(expected)
+})
+
